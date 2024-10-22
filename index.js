cdf571dda1478ddf7a66b25179d698ab352cb040
peter <mutisyapeter336@gmail.com>
2024-10-22 09:30:17 +0300

    added a new file

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // code your solution here
+function superbowlWin(record) {
+  // Use find() to look for the first winning record
+  const win = record.find(entry => entry.result === "W");
+  // Return the year if a win is found, otherwise return undefined
+  return win ? win.year : undefined;
+}
